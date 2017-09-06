b3176b866f515b8aade222113e4e9d9549771e70
hirose_y <hirose.hy@gmail.com>
2017-09-06 17:41:23 +0900

    googlemap

--- project1-frontend-app/addJson.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $fp = fopen("../place.json", "a");
-    $line = '{"name": "'.$_POST["placeName"].'","lat":'.$_POST["jsonLat"].', "lng": '.$_POST["jsonLng"].',"type": "'.$_POST["jsonType"].'","comment": "'.$_POST["jsonComment"].'"}';
-    fwrite( $fp, ",\n".$line );
-    fclose( $fp );
-    header("Location: http://localhost/googlemaps/ver1/index.php");
-?>
--- project1-frontend-app/addPlace.php
+++
@@ -1,8 +0,0 @@
-<?php
-//if(isset($_POST["sub1"])){
-    $fp = fopen("../place.json", "a");
-    $line = {"name": }
-    fwrite( $fp, $line."\n" );
-    fclose( $fp );
-//}
-?>
